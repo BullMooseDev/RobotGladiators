03f9a3ef8c6f7a23781e75a0b83b9f8a950c6520
thatblondetitan <koleervine@gmail.com>
2021-12-20 18:40:32 -0700

    3.1.6 update

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,23 @@
-window.alert("This is an alert! JavaScript is running!");
+// this creates a function named "fight"
+
+  function fight() {
+      window.alert("the fight has begun!");
+  }
+
+  var playerName = window.prompt("What is your robots name");
+  // what is this?
+  console.log(playerName);
+
+  console.log("this logs a string, good for leaving yourself a message");
+  // this will do math and log 20
+  console.log(10 + 10);
+  // what is this?
+  console.log("our robots name is " + playerName);
+
+  var moniker = "your name";
+  console.log(moniker);
+
+
+
+
+  //fight();   or   /* fight(); */
